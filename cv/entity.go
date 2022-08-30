99f23ce115b01e5d7e5585fad7c8a2fb67e4d31a
Baihaki123 <baihakiaski@gmail.com>
2022-08-30 14:18:33 +0700

    add entity or struct resume

--- cv/entity.go
+++ cv/entity.go
@@ -1,1 +1,26 @@
 package cv
+
+import "time"
+
+type Resume struct {
+	ID                    int
+	Name                  string
+	Email                 string
+	PhoneNumber           string
+	LinkedinURL           string
+	PortofolioURL         string
+	CompanyName           string
+	OccupationPosition    string
+	OccupationStart       time.Time
+	OccupationEnd         time.Time
+	OccupationStatus      string
+	OccupationAchievement string
+	EducationName         string
+	EducationDegree       string
+	EducationFaculty      string
+	EducationCity         string
+	EducationStart        time.Time
+	EducationEnd          time.Time
+	EducationScore        float32
+	Achievements          string
+}
