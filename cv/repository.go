064a7a61cefabdd0d2e642634767eceaca0c83db
Baihaki123 <baihakiaski@gmail.com>
2022-08-30 15:22:43 +0700

    add repository

--- cv/repository.go
+++ cv/repository.go
@@ -1,1 +1,67 @@
 package cv
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	GetAll() ([]Resume, error)
+	GetByID(ID int) (Resume, error)
+	Save(resume Resume) (Resume, error)
+	Update(resume Resume) (Resume, error)
+	Delete(resume Resume) error
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) GetAll() ([]Resume, error) {
+	var resume []Resume
+	err := r.db.Find(&resume).Error
+	if err != nil {
+		return resume, err
+	}
+
+	return resume, nil
+}
+
+func (r *repository) GetByID(ID int) (Resume, error) {
+	var resume Resume
+
+	err := r.db.Where("id = ?", ID).Find(&resume).Error
+	if err != nil {
+		return resume, err
+	}
+
+	return resume, nil
+}
+
+func (r *repository) Save(resume Resume) (Resume, error) {
+	err := r.db.Create(&resume).Error
+	if err != nil {
+		return resume, err
+	}
+
+	return resume, nil
+}
+
+func (r *repository) Update(resume Resume) (Resume, error) {
+	err := r.db.Save(&resume).Error
+	if err != nil {
+		return resume, err
+	}
+
+	return resume, nil
+}
+
+func (r *repository) Delete(resume Resume) error {
+	err := r.db.Delete(&resume).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module resume
 
 go 1.17
 
-require github.com/mattn/go-sqlite3 v1.14.15
+require (
+	github.com/mattn/go-sqlite3 v1.14.15
+	gorm.io/gorm v1.23.8
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
